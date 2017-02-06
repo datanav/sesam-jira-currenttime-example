b1b19958e2d1986334522a29ea345d0e18940a3e
Knut Johannessen <knut.johannessen@bouvet.no>
2017-02-06 11:02:27 +0100

    code style fixes

--- webserver/webserver/api/__init__.py
+++ webserver/webserver/api/__init__.py
@@ -1,12 +1,13 @@
-import collections
-from flask import render_template, request, current_app
 import pathlib
 
-from .jira_project_component_mappings.csv import get_jira_project_component_mapping_results
-from .customfield_mappings.csv import get_customfield_mapping_results
+from flask import render_template, current_app
+
 from .compare_totals.csv import get_totals_searchresults
 from .currenttime_with_jira_keys.csv import get_current_time_with_jira_keys_results
+from .customfield_mappings.csv import get_customfield_mapping_results
+from .jira_project_component_mappings.csv import get_jira_project_component_mapping_results
 from .utils import get_search_results_by_query, assert_user_name_from_request
+
 
 def get_administered_currenttime_subtasks():
     # NOTE: the CurrentTime web gui labels the task and subtask as 'Subproject' and 'Task', so that is what we ",
@@ -14,7 +15,6 @@ def get_administered_currenttime_subtasks():
     #  project => 'Project'",
     #  task => 'Subproject'",
     #  subtask => 'Task'"
-
 
     user_name = assert_user_name_from_request()
     # it seems that elasticsearch indexes on lowercase
@@ -38,8 +38,11 @@ def get_administered_currenttime_subtasks():
             }
         })
 
-    administered_subtasks.sort(key=lambda subtask: (subtask["projectname"], subtask["taskname"], subtask["subtaskname"]))
+    administered_subtasks.sort(key=lambda subtask: (subtask["projectname"],
+                                                    subtask["taskname"],
+                                                    subtask["subtaskname"]))
     return administered_subtasks
+
 
 def get():
     swagger_file_url = pathlib.PurePosixPath("/", current_app.config["virtualhost_path"],
@@ -49,8 +52,7 @@ def get():
     current_time_with_jira_keys_results = get_current_time_with_jira_keys_results()[1]
     customfield_mapping_results = get_customfield_mapping_results()[1]
     jira_project_component_mapping_results = get_jira_project_component_mapping_results()[1]
-    administered_currenttime_subtasks  = get_administered_currenttime_subtasks()
-
+    administered_currenttime_subtasks = get_administered_currenttime_subtasks()
 
     return render_template("frontpage.html",
                            swagger_file_url=swagger_file_url,
@@ -59,5 +61,5 @@ def get():
                            customfield_mapping_results=customfield_mapping_results,
                            jira_project_component_mapping_results=jira_project_component_mapping_results,
                            administered_currenttime_subtasks=administered_currenttime_subtasks,
-                           user_name = assert_user_name_from_request()
+                           user_name=assert_user_name_from_request()
                            )
